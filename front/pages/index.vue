cdef404a51fceda9b26f60f5d4f7f299eb299bc5
kei-600th <kei19941005@gmail.com>
2023-04-28 19:25:48 +0900

    TODOの登録のリクエストを送り、保存されるのを確認してみる

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -8,6 +8,7 @@
 <script>
   import AddTodo from "@/components/AddTodo";
   import TodoList from "@/components/TodoList";
+  import axios from "@/plugins/axios";  // 追加
 
   export default {
     components: {
@@ -20,14 +21,14 @@
       };
     },
     methods: {
-      addTodo(title) {
+      async addTodo(title) { // asyncを追加
+        await axios.post("/v1/todos", { title }); // この行を追加
         this.todos.push({
           title
         });
       },
     },
   };
-
 </script>
 
 <style>
